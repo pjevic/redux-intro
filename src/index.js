27712aaab140892fff10b00dac93c5811554d178
Milan Pjevic <milanpjevic@icloud.com>
2024-12-01 10:48:41 +0100

    Professional Redux File Structure: State Slices

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-import "./Store";
+import store from "./Store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
